55603584dc71756dc4f48663b1bdc72694fc28c0
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-20 00:42:36 -0500

    JournalPage with Views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,28 @@
-import { Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
-        unde deserunt eius expedita praesentium quas culpa tempora asperiores
-        alias molestias.
-      </Typography>
+      {/* <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium unde deserunt eius expedita praesentium quas culpa tempora asperiores alias molestias.</Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
