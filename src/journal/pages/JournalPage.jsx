10444a2bc4c3fedbb44cd23ac5b48a285cd880b7
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-19 23:56:51 -0500

    Journal Pages

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+        unde deserunt eius expedita praesentium quas culpa tempora asperiores
+        alias molestias.
+      </Typography>
+    </JournalLayout>
+  );
+};
