04bd60b3843db1228c7afc7c5446af4dc9938ddb
obidev <andersonlosada.11@gmail.com>
2025-06-01 22:55:31 -0500

    class 303

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,8 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
-import { addNewEmptyNote, savingNewNote, setActiveNote } from './';
+import { addNewEmptyNote, setActiveNote } from './';
+import { savingNewNote, setNotes } from './journalSlice';
+import { loadNotes } from '../../helpers';
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -21,5 +23,15 @@ export const startNewNote = () => {
 
     dispatch(addNewEmptyNote(newNote));
     dispatch(setActiveNote(newNote));
+  };
+};
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    if (!uid) throw new Error('El UID del usuario no existe');
+
+    const notes = await loadNotes(uid);
+    dispatch(setNotes(notes));
   };
 };
